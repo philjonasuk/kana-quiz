e0c1c045b8ddb9c93cb23c0b2288683da6af4179
wholetonegames <105543918+philjonasuk@users.noreply.github.com>
2022-11-11 23:06:44 +0000

    works

---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { App } from "./containers/App";
+import { HashRouter as Router } from "react-router-dom";
+import { createRoot } from 'react-dom/client';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Router basename={process.env.PUBLIC_URL}>
+//       <App />
+//     </Router>
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
+
+const container = document.getElementById('app');
+const root = createRoot(container!); // createRoot(container!) if you use TypeScript
+root.render(<App />);
