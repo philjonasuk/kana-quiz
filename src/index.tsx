675558f25cd419afc0a00075d77364f17a29e2c8
wholetonegames <105543918+philjonasuk@users.noreply.github.com>
2022-11-12 00:13:20 +0000

    lint

--- src/index.tsx
+++ src/index.tsx
@@ -14,4 +14,4 @@ import { createRoot } from 'react-dom/client';
 
 const container = document.getElementById('app');
 const root = createRoot(container!); // createRoot(container!) if you use TypeScript
-root.render(<App />);
+root.render(<Router basename={process.env.PUBLIC_URL}><App /></Router>);
