35460451f28e05f5444cae7466f391c6778177d0
philjonasuk <105543918+philjonasuk@users.noreply.github.com>
2022-12-03 20:22:46 +0000

    Update context.tsx

--- src/data/context.tsx
+++ src/data/context.tsx
@@ -1,13 +1,29 @@
-import React, {createContext, useContext, useMemo, useState} from 'react';
+import React, {createContext, useContext, useEffect, useMemo, useState} from 'react';
+import {shuffleArray} from '../utils/shuffleArray';
+import {kana_list} from './kana_list';
 import {KanaType, KanaContextType} from './types';
 
 export const KanaContext = createContext<KanaContextType>({} as KanaContextType);
 
 export const KanaProvider: React.FC<any> = ({children}) => {
   const [kana, setKana] = useState<KanaType>('h');
+  const [previousKana, setPreviousKana] = useState<KanaType | null>(null);
   const kanaLabel = useMemo(() => (kana === 'k' ? 'Katakana' : 'Hiragana'), [kana]);
+  const [shuffledKanaIds, setShuffledKanaIds] = useState<string[]>([]);
+
+  useEffect(() => {
+    // only create a new shuffled array if kana type has changed
+    if (kana === previousKana) {
+      return;
+    }
+    setShuffledKanaIds(
+      shuffleArray(kana_list.filter((x) => x.type === kana).map((x) => x.id))
+    );
+    setPreviousKana(kana);
+  }, [kana, previousKana]);
+
   return (
-    <KanaContext.Provider value={{kana, setKana, kanaLabel}}>
+    <KanaContext.Provider value={{kana, setKana, kanaLabel, shuffledKanaIds}}>
       {children}
     </KanaContext.Provider>
   );
