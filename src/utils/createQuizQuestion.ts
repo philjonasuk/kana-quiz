0163e50a9915539968aeef1f3790f26a2df103aa
philjonasuk <105543918+philjonasuk@users.noreply.github.com>
2022-12-03 21:16:08 +0000

    Update createQuizQuestion.ts

--- src/utils/createQuizQuestion.ts
+++ src/utils/createQuizQuestion.ts
@@ -1,21 +1,29 @@
 import {QuizQuestion} from '../data/types';
+import {shuffleArray} from './shuffleArray';
+
+const getRandomElement = (arr: any[]) => arr[Math.floor(Math.random() * arr.length)];
 
 export const createQuizQuestion = (
   quizIndex: number,
   shuffledKanaIds: string[]
-): QuizQuestion => {
-  const answerId = shuffledKanaIds[quizIndex];
-  const questionIds: string[] = [];
+): QuizQuestion | undefined => {
+  if (shuffledKanaIds.length === 0) {
+    return undefined;
+  }
 
-  while (questionIds.length < 4) {
-    let randomId = questionIds[Math.floor(Math.random() * questionIds.length)];
+  const answerId = shuffledKanaIds[quizIndex];
+  const questionIds: string[] = [answerId];
+
+  for (let i = 0; i < 3; i += 1) {
+    let randomId = getRandomElement(shuffledKanaIds);
     while (randomId === answerId) {
-      randomId = questionIds[Math.floor(Math.random() * questionIds.length)];
+      randomId = getRandomElement(shuffledKanaIds);
     }
     questionIds.push(randomId);
   }
+
   return {
     answerId,
-    questionIds,
+    questionIds: shuffleArray(questionIds),
   };
 };
