768b148dc0d37ff563ce3584351a4ef4b5229fa3
philjonasuk <105543918+philjonasuk@users.noreply.github.com>
2022-12-03 21:38:29 +0000

    Update Quiz.tsx

--- src/containers/Quiz.tsx
+++ src/containers/Quiz.tsx
@@ -1,11 +1,42 @@
+import {
+  Box,
+  FormControl,
+  FormControlLabel,
+  Radio,
+  RadioGroup,
+  Typography,
+} from '@mui/material';
 import React from 'react';
-import {useParams} from 'react-router-dom';
+import {useKanaContext} from '../data/context';
+import {useSetKana} from '../hooks/useSetKana';
 
 export const Quiz: React.FC = () => {
-  const {kana, seed, index} = useParams();
+  const {kanaLabel, quizQuestion} = useKanaContext();
+  console.log({quizQuestion});
+  useSetKana();
   return (
-    <div>
-      Quiz {kana} {seed} {index}
-    </div>
+    <Box>
+      <Typography variant="h3">{kanaLabel} Quiz</Typography>
+      <Typography variant="h1" ml="auto" mr="auto">
+        {quizQuestion?.answer.kana}
+      </Typography>
+      <Box>
+        <FormControl>
+          <RadioGroup>
+            {quizQuestion?.questions.map((question) => (
+              <FormControlLabel
+                key={question.id}
+                value={question.id}
+                control={<Radio />}
+                label={question.reading}
+                onChange={() => {
+                  // setKana('h');
+                }}
+              />
+            ))}
+          </RadioGroup>
+        </FormControl>
+      </Box>
+    </Box>
   );
 };
