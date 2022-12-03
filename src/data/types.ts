e32c5246e967d2e5bdfd010ecad667c1986b6a8a
philjonasuk <105543918+philjonasuk@users.noreply.github.com>
2022-12-03 21:16:36 +0000

    Update types.ts

--- src/data/types.ts
+++ src/data/types.ts
@@ -1,10 +1,18 @@
 export type KanaType = 'k' | 'h';
 
+export interface QuizQuestion {
+  answerId: string;
+  questionIds: string[];
+}
+
 export interface KanaContextType {
   kana: KanaType;
   setKana: React.Dispatch<React.SetStateAction<KanaType>>;
   kanaLabel: string;
   shuffledKanaIds: string[];
+  quizIndex: number;
+  quizQuestion: QuizQuestion | undefined;
+  incrementQuizQuestion: () => void;
 }
 
 export interface KanaCharacter {
