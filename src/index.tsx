48a5ee742e347c1aad29f60ba6231609aff55eef
wholetonegames <105543918+philjonasuk@users.noreply.github.com>
2022-12-04 22:49:37 +0000

    another try

--- src/index.tsx
+++ src/index.tsx
@@ -6,11 +6,12 @@ import {Review} from './containers/Review';
 import {Quiz} from './containers/Quiz';
 import {KanaProvider} from './data/context';
 
+const basename = document.querySelector('base')?.getAttribute('href') ?? '/';
 const container = document.getElementById('app');
 const root = createRoot(container!);
 root.render(
   <KanaProvider>
-    <HashRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter basename={basename}>
       <Routes>
         <Route path="/quiz/:kana" element={<Quiz />} />
         <Route path="/review/:kana" element={<Review />} />
