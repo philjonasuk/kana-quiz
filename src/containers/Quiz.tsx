0d578472bae8f1eb24535f35bd1369c873402a0e
philjonasuk <105543918+philjonasuk@users.noreply.github.com>
2022-12-04 11:11:51 +0000

    Update Quiz.tsx

--- src/containers/Quiz.tsx
+++ src/containers/Quiz.tsx
@@ -1,41 +1,94 @@
 import {
   Box,
+  Button,
   FormControl,
   FormControlLabel,
+  FormHelperText,
   Radio,
   RadioGroup,
   Typography,
 } from '@mui/material';
-import React from 'react';
+import React, {useState} from 'react';
+import {useNavigate} from 'react-router-dom';
 import {useKanaContext} from '../data/context';
+import {AnswerType} from '../data/types';
 import {useSetKana} from '../hooks/useSetKana';
 
 export const Quiz: React.FC = () => {
-  const {kanaLabel, quizQuestion} = useKanaContext();
-  console.log({quizQuestion});
+  const navigate = useNavigate();
+  const {
+    kanaLabel,
+    quizQuestion,
+    incrementQuizQuestion,
+    correctAnswersCount,
+    shuffledKanaIds,
+    quizIndex,
+    resetQuiz,
+  } = useKanaContext();
+  const [chosenId, setChosenId] = useState<string | null>(null);
+  const [answerType, setAnswerType] = useState<AnswerType>('unanswered');
   useSetKana();
   return (
     <Box>
       <Typography variant="h3">{kanaLabel} Quiz</Typography>
+      <Typography variant="h5">
+        Current: {quizIndex + 1} / Correct: {correctAnswersCount} / Total:{' '}
+        {shuffledKanaIds.length}
+      </Typography>
       <Typography variant="h1" ml="auto" mr="auto">
         {quizQuestion?.answer.kana}
       </Typography>
+      <FormControl>
+        <RadioGroup value={chosenId} style={{display: 'flex', flexDirection: 'row'}}>
+          {quizQuestion?.questions.map((question) => (
+            <FormControlLabel
+              key={question.id}
+              value={question.id}
+              control={<Radio />}
+              label={question.reading}
+              disabled={answerType !== 'unanswered'}
+              onChange={() => {
+                setChosenId(question.id);
+                setAnswerType(
+                  question.id === quizQuestion?.answer.id ? 'correct' : 'wrong'
+                );
+              }}
+            />
+          ))}
+        </RadioGroup>
+        {answerType === 'wrong' && (
+          <FormHelperText>
+            The correct answer is : {quizQuestion?.answer.reading}
+          </FormHelperText>
+        )}
+      </FormControl>
       <Box>
-        <FormControl>
-          <RadioGroup>
-            {quizQuestion?.questions.map((question) => (
-              <FormControlLabel
-                key={question.id}
-                value={question.id}
-                control={<Radio />}
-                label={question.reading}
-                onChange={() => {
-                  // setKana('h');
-                }}
-              />
-            ))}
-          </RadioGroup>
-        </FormControl>
+        {answerType !== 'unanswered' && (
+          <Button
+            variant="contained"
+            onClick={() => {
+              setChosenId(null);
+              setAnswerType('unanswered');
+              incrementQuizQuestion(answerType === 'correct');
+            }}
+          >
+            {`${
+              answerType === 'correct'
+                ? 'Correct! next question'
+                : 'Wrong... next question'
+            }`}
+          </Button>
+        )}
+      </Box>
+      <Box>
+        <Button
+          onClick={() => {
+            resetQuiz();
+            navigate(`/`);
+          }}
+        >
+          Back to main page
+        </Button>
       </Box>
     </Box>
   );
