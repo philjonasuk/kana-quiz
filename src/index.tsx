c4638cd9baf15b3d1f2b37c808574ce2caafe1b5
wholetonegames <105543918+philjonasuk@users.noreply.github.com>
2022-12-04 23:43:19 +0000

    tsconfig

--- src/index.tsx
+++ src/index.tsx
@@ -6,16 +6,16 @@ import {Review} from './containers/Review';
 import {Quiz} from './containers/Quiz';
 import {KanaProvider} from './data/context';
 
-const basename = document.querySelector('base')?.getAttribute('href') ?? '/';
+// const basename = document.querySelector('base')?.getAttribute('href') ?? '/';
 const container = document.getElementById('app');
 const root = createRoot(container!);
 root.render(
   <KanaProvider>
-    <HashRouter basename={basename}>
+    <HashRouter basename="/">
       <Routes>
-        <Route path="/quiz/:kana" element={<Quiz />} />
-        <Route path="/review/:kana" element={<Review />} />
-        <Route path="/*" element={<MainMenu />} />
+        <Route path="quiz/:kana" element={<Quiz />} />
+        <Route path="review/:kana" element={<Review />} />
+        <Route path="*" element={<MainMenu />} />
       </Routes>
     </HashRouter>
   </KanaProvider>
