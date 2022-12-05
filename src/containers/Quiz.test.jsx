9804b0a664dd4d8c045abf171d2c69a341aa23f4
wholetonegames <105543918+philjonasuk@users.noreply.github.com>
2022-12-05 00:02:45 +0000

    tests pass

--- src/containers/Quiz.test.jsx
+++ src/containers/Quiz.test.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import {render} from '@testing-library/react';
 import {Quiz} from './Quiz';
+import {KanaProvider} from '../data/context';
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: () => jest.fn(),
+}));
 
 describe('Quiz', () => {
   it('renders', () => {
-    render(<Quiz />);
+    render(
+      <KanaProvider>
+        <Quiz />
+      </KanaProvider>
+    );
   });
 });
--- src/containers/Review.test.jsx
+++ src/containers/Review.test.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import {render} from '@testing-library/react';
 import {Review} from './Review';
 
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: () => jest.fn(),
+}));
+
 describe('Review', () => {
   it('renders', () => {
     render(<Review />);
